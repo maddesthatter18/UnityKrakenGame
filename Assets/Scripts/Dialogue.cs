0e7e2bd02b67839a26ee097fd2c86f51f071fc55
maddesthatter18 <112992172+maddesthatter18@users.noreply.github.com>
2023-12-05 13:26:41 -0600

    Reworking Dialogue System
    Making it cleaner and more optimized.

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -1,65 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+[System.Serializable]
+public class Dialogue: MonoBehaviour {
 
-public class Dialogue : MonoBehaviour
-{
-   public TextMeshProUGUI textComponent;
-   public string[] lines;
-   [SerializeField] public float textSpeed;
-   [SerializeField] public string _name;
-    private int index;
-    // Start is called before the first frame update
-    void Start()
-    {
-        textComponent.text = string.Empty;
-        StartDialogue();
-    }
+    public string _name;
+    public string[] sentences;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            if(textComponent.text == lines[index])
-            {
-                NextLine();
-            }
-            else
-            {
-                StopAllCoroutines();
-                textComponent.text = lines[index];
-            }
-        }
-    }
-
-    void StartDialogue()
-    {
-        index = 0;
-        StartCoroutine(TypeLine());
-    }
-
-    IEnumerator TypeLine()
-    {
-        foreach (char c in lines[index].ToCharArray())
-        {
-            textComponent.text += c;
-            yield return new WaitForSeconds(textSpeed);
-        }
-    }
-
-    void NextLine()
-    {
-        if (index < lines.Length - 1)
-        {
-            index++;
-            textComponent.text = string.Empty;
-            StartCoroutine(TypeLine());
-        }
-        else
-        {
-            gameObject.SetActive(false);
-        } 
-    }
 }
